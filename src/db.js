6cd4491df1479e172b62637c3ef4c1d4b6a20583
i-Lucas <novo.contato.lucas@gmail.com>
2022-07-07 13:40:19 -0300

    first commit

---
+++ src/db.js
@@ -0,0 +1,26 @@
+import pg from "pg";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const { Pool } = pg;
+
+const config = {
+    connectionString: process.env.DATABASE_URL
+}
+/*
+if (process.env.MODE === "PROD") {
+    config.ssl = {
+        rejectUnauthorized: false
+    }
+}
+*/
+const connection = new Pool(config);
+
+connection.query("SELECT NOW()", (err, res) => {
+
+    if (res) { console.log(`connected to database at ${res.rows[0].now}`); }
+    else { console.log('failed to connect to database'); }
+})
+
+export default connection;
